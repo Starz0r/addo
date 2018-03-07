f77cdc99d41915681f2bd91e012b3e9ab9e63c57
Starz0r <starz0rdesign@gmail.com>
2018-03-06 23:59:20 -0600

    New function to handle executing shell commands and waiting on them

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ use winapi::um::processthreadsapi;
 use winapi::um::winbase;
 use winapi::um::processenv;
 use winapi::um::errhandlingapi::GetLastError;
+use winapi::um::shellapi;
 
 fn main()
 {
@@ -98,6 +99,44 @@ fn main()
 
         println!("Last Error: {:?}", GetLastError());
     }
+}
+
+unsafe fn ShellExecuteAndWait(lpOperation: String, lpFile: String, lpParameters: String, lpDirectory: String, nShowCmd: i32) -> Result<u32, &'static str>
+{
+    let operationWide: Vec<u16> = OsStr::new(&lpOperation)
+        .encode_wide()
+        .chain(once(0))
+        .collect();
+
+    let parametersWide: Vec<u16> = OsStr::new(&lpParameters)
+        .encode_wide()
+        .chain(once(0))
+        .collect();
+
+    let directoryWide: Vec<u16> = OsStr::new(&lpDirectory)
+        .encode_wide()
+        .chain(once(0))
+        .collect();
+
+    let fileWide: Vec<u16> = OsStr::new(&lpFile)
+        .encode_wide()
+        .chain(once(0))
+        .collect();
+
+    let mut info = shellapi::SHELLEXECUTEINFOW2
+    {
+        cbSize: 0,
+        fMask: shellapi::SEE_MASK_NOASYNC | shellapi::SEE_MASK_NOCLOSEPROCESS,
+        hwnd: ptr::null_mut(),
+        lpVerb: operationWide.as_ptr(),
+        lpFile: fileWide.as_ptr(),
+        lpParameters: parametersWide.as_ptr(),
+        lpDirectory: directoryWide.as_ptr(),
+        nShow: nShowCmd,
+    };
+    info.cbSize = size_of_val(&info).count_zeros();
+    shellapi::ShellExecuteExW2(&mut info);
+    return Ok(0);
 }
 
 fn find_in_path<P: AsRef<Path>>(name: P) -> Option<PathBuf> {
